ee57402b5bf4d4b6fe1f74c2d2925032b8fe1f2f
Caleb Dinsmore <calebjdinsmore@gmail.com>
2019-04-04 21:44:02 -0400

    Made the RegEx expression more flexible and added times for vague dates (#132)

--- plugins/pco/teams.py
+++ plugins/pco/teams.py
@@ -9,15 +9,16 @@ pco = pypco.PCO(os.environ["WILL_PCO_APPLICATION_KEY"], os.environ["WILL_PCO_API
 attachment_list = []
 
 
-def get_for_date(team, plan_date):
+def get_for_date(team, date_expression):
     print('Looking up the team: ', team)
     cal = parsedatetime.Calendar()
-    now_hour, now_minute = datetime.now().hour, datetime.now().minute
-    plan_date, parse_status = cal.parse(plan_date)
+    now = datetime.now().astimezone().astimezone(pytz.utc)
+    now_hour, now_minute = now.hour, now.minute
+    plan_date, parse_status = cal.parse(date_expression)
     plan_date = datetime(*plan_date[:6]).astimezone().astimezone(pytz.utc)
     is_specific = False
     start_time = datetime(month=plan_date.month, day=plan_date.day, year=plan_date.year)
-    if plan_date.hour != now_hour or plan_date.minute != now_minute:
+    if date_expression != 'today' and (plan_date.hour != now_hour or plan_date.minute != now_minute):
         # parsedatetime by default creates a datetime matching the current hour and minute if not provided
         # is_specific is True if the parsed string contains a specific time, e.g. "Sunday at 9AM"
         is_specific = True
@@ -26,21 +27,22 @@ def get_for_date(team, plan_date):
                               year=plan_date.year,
                               hour=plan_date.hour,
                               minute=plan_date.minute)
+    start_time = pytz.utc.localize(start_time)
     team_members = {
         'confirmed': [],
         'unconfirmed': [],
-        'declined': [],
     }
     for t in pco.services.teams.list(where={'name': team}, include='people'):
         for member in t.rel.people.list():
             for plan_person in member.rel.plan_people.list():
                 for plan_time in plan_person.rel.plan_times.list():
-                    starts_at = datetime.strptime(plan_time.starts_at, '%Y-%m-%dT%H:%M:%SZ')
-                    if starts_at == start_time:
+                    starts_at = pytz.utc.localize(datetime.strptime(plan_time.starts_at, '%Y-%m-%dT%H:%M:%SZ'))
+                    hour_format = starts_at.astimezone().strftime('%I:%M%p')
+                    if dates_match(starts_at, start_time, is_specific):
                         if plan_person.status in ('C', 'Confirmed'):
-                            team_members['confirmed'].append(plan_person)
+                            team_members['confirmed'].append((plan_person, hour_format))
                         elif plan_person.status in ('U', 'Unconfirmed'):
-                            team_members['unconfirmed'].append(plan_person)
+                            team_members['unconfirmed'].append((plan_person, hour_format))
     if is_specific:
         plan_date = plan_date.astimezone()  # Convert to local timezone
         plan_date_format = plan_date.strftime('%m-%d-%Y at %I:%M%p')
@@ -52,8 +54,10 @@ def get_for_date(team, plan_date):
         msg = '{} team members scheduled for *{}*:'.format(team.capitalize(), plan_date_format)
         for status in ['confirmed', 'unconfirmed']:
             msg += '\n\n{}:'.format(status.capitalize())
-            for plan_person in team_members[status]:
-                msg += '\n- {}'.format(plan_person.name)
+            for plan_person_tuple in team_members[status]:
+                msg += '\n- {}'.format(plan_person_tuple[0].name)
+                if not is_specific:
+                    msg += ' ({})'.format(plan_person_tuple[1])
     attachment_list.append(msg_attachment.
                            SlackAttachment(fallback=msg, text=msg))
     return attachment_list
@@ -107,6 +111,13 @@ def get_team_assignments(team):
         print("No team given")
 
     return
+
+
+def dates_match(date_one, date_two, is_specific):
+    if is_specific:
+        return date_one == date_two
+    else:
+        return date_one.day == date_two.day and date_one.month == date_two.month and date_one.year == date_two.year
 
 
 if __name__ == '__main__':
